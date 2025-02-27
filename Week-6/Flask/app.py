d111885f9dfdf71e3e5abb5547217f53b928b327
faswallex <oifasanya@gmail.com>
2025-02-27 00:53:44 -0330

    WEEK-7 EXAMPLE

--- Week-6/Flask/app.py
+++ Week-6/Flask/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
+
 def home():
-    return 'Hello, Flask!'
+    return render_template('index.html')
+
+@app.route('/contact')
+
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about')
+
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
