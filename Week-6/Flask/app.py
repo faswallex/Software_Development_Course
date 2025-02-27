f1e21d49ba4fdf60beced72bcc5121c4340afbf5
faswallex <oifasanya@gmail.com>
2025-02-27 00:57:47 -0330

    Flask basic routing

--- Week-6/Flask/app.py
+++ Week-6/Flask/app.py
@@ -2,20 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
-
 def home():
-    return render_template('index.html')
-
-@app.route('/contact')
-
-def contact():
-    return render_template('contact.html')
+    return "Welcome to My Flask App!"
 
 @app.route('/about')
-
 def about():
-    return render_template('about.html')
+    return "This is the About Page"
 
-if __name__ == '__main__':
-   app.run(debug=True)
+@app.route('/contact')
+def contact():
+    return "Contact us at contact@example.com"
+
+if __name__ == '__main__':    app.run(debug=True)
