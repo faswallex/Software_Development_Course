afe5aee4d3c0d5175c3b0a48dd48ac3fa1420095
faswallex <oifasanya@gmail.com>
2025-03-09 10:29:18 -0230

    Setting up a basic Flask app

--- Week-6/Flask/app.py
+++ Week-6/Flask/app.py
@@ -1,18 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to My Flask App!"
-
-@app.route('/about')
-def about():
-    return "This is the About Page"
-
-@app.route('/contact')
-def contact():
-    return "Contact us at contact@example.com"
+    return "Hello, Flask!"
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
