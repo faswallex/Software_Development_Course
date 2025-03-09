5bab7e6761e8faa90fb011973427db3f9db48bdc
faswallex <oifasanya@gmail.com>
2025-03-09 11:01:47 -0230

    Flask basic routing

---
+++ Week-7/PART1/Flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to My Flask App!"
+
+@app.route('/about')
+def about():
+    return "This is the About Page"
+
+@app.route('/contact')
+def contact():
+    return "Contact us at contact@example.com"
+
+if __name__ == '__main__':
+    app.run(debug=True)
